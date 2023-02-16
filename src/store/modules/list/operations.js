9faa34d035dc8af1ed013bbe0f4cab95205054aa
Movses5555 <mosisrayelyan@gmail.com>
2023-02-16 21:59:41 +0400

    redux loading state finished

--- src/store/modules/list/operations.js
+++ src/store/modules/list/operations.js
@@ -1,87 +1,65 @@
 import * as actions from "./actions";
 import sleep from "helpers/sleep";
 import toast from "helpers/toast";
-export const createNewList = (data) => {
-  return async (dispatch) => {
-    dispatch(actions.createListStart());
-    try {
-      await sleep();
-      dispatch(actions.createListCompleted(data));
-      toast.success("List successfully created");
-    } catch (error) {
-      toast.error("Something went wrong");
-      console.error(error);
-    }
-  };
+import { requestsEnum } from "../requestsEnum";
+import { requestHelper } from "../request/operations";
+
+export const createNewList = (data) => async (dispatch) => {
+  return await requestHelper(dispatch, requestsEnum().createList, async () => {
+    await sleep();
+    dispatch(actions.createListCompleted(data));
+    toast.success("List was successfully created!");
+  });
 };
 
-export const editListOperation = (data) => {
-  return async (dispatch) => {
-    dispatch(actions.editListStart());
-    try {
-      await sleep();
-      dispatch(actions.editListCompleted(data));
-      toast.success("Title was successfully edited");
-    } catch (error) {
-      toast.error("Something went wrong");
-      console.error(error);
-    }
-  };
+export const editListOperation = (data) => async (dispatch) => {
+  return await requestHelper(dispatch, requestsEnum().editList, async () => {
+    await sleep();
+    dispatch(actions.editListCompleted(data));
+    toast.success("List title was successfully edited!");
+  });
 };
 
-export const deleteListOperation = (id) => {
-  return async (dispatch) => {
-    dispatch(actions.deleteListStart());
-    try {
+export const deleteListOperation = (id) => async (dispatch) => {
+  return await requestHelper(
+    dispatch,
+    requestsEnum(id).deleteList,
+    async () => {
       await sleep();
       dispatch(actions.deleteListCompleted(id));
-      toast.success("List was successfully deleted");
-    } catch (error) {
-      toast.error("Something went wrong");
-      console.error(error);
+      toast.success("List was successfully deleted!");
     }
-  };
+  );
 };
 
-export const createNewCardOperation = (data) => {
-  return async (dispatch) => {
-    dispatch(actions.createCardStart());
-    try {
-      await sleep();
-      dispatch(actions.createCardCompleted(data));
-      toast.success("Card was successfully created");
-    } catch (error) {
-      toast.error("Something went wrong");
-      console.error(error);
-    }
-  };
+export const createNewCardOperation = (data) => async (dispatch) => {
+  return await requestHelper(dispatch, requestsEnum().createCard, async () => {
+    await sleep();
+    dispatch(actions.createCardCompleted(data));
+    toast.success("Card was successfully created!");
+  });
 };
 
-
-export const deleteCardOperation = (data) => {
-  return async (dispatch) => {
-    dispatch(actions.deleteCardStart());
-    try {
+export const deleteCardOperation = (data) => async (dispatch) => {
+  return await requestHelper(
+    dispatch,
+    requestsEnum(data.id).deleteCard,
+    async () => {
       await sleep();
       dispatch(actions.deleteCardCompleted(data));
-      toast.success("Card was successfully deleted");
-    } catch (error) {
-      toast.error("Something went wrong");
-      console.error(error);
+      toast.success("Card was successfully deleted!");
     }
-  };
+  );
 };
 
-export const editCardOperation = (data) => {
-  return async (dispatch) => {
-    dispatch(actions.editCardStart());
-    try {
+export const editCardOperation = (data) => async (dispatch) => {
+  return await requestHelper(
+    dispatch,
+    requestsEnum(data.id).editCard,
+    async () => {
       await sleep();
       dispatch(actions.editCardCompleted(data));
-      toast.success("Card was successfully Edited");
-    } catch (error) {
-      toast.error("Something went wrong");
-      console.error(error);
+      toast.success("Card was successfully edited!");
     }
-  };
+  );
 };
---
+++ src/store/modules/request/operations.js
@@ -0,0 +1,12 @@
+import * as actions from "./actions";
+
+export const requestHelper = async (dispatch, requestName, request) => {
+  console.log(requestName);
+  dispatch(actions.requestStarted(requestName));
+  try {
+    await request();
+    dispatch(actions.requestFinished(requestName));
+  } catch (error) {
+    console.error(error);
+  }
+};
